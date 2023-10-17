f2a24549be39926f4a34dc158d7e2f479a3d1a20
morris menanya <emmiemenz@gmail.com>
2023-10-17 19:17:42 +0100

    clear linters

--- src/components/nav-bar/SideBar.jsx
+++ src/components/nav-bar/SideBar.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { AiOutlineClose, AiOutlineMenu, AiOutlineSearch } from 'react-icons/ai';
 import { HiOutlineLocationMarker, HiOutlineSwitchVertical } from 'react-icons/hi';
-import {BiCurrentLocation} from "react-icons/bi"
+import { BiCurrentLocation } from 'react-icons/bi';
 import { useDispatch, useSelector } from 'react-redux';
 import { setToggleMenu } from '../../redux/toggleMenu/toggle';
 
@@ -23,31 +23,31 @@ const SideBar = () => {
             Driving directions
           </h3>
         </div>
-        <div className='flex'>
-        
-            <div className="mt-3 flex-1">
-                <div className="flex items-center justify-between gap-1 ">
-                    <span><HiOutlineLocationMarker className="text-blue-800" /></span>
-                    <div className="flex-1 rounded-md overflow-hidden relative">
-                    <input type="text" className="border text-xs p-3 w-full  bg-gray-100  outline-none" placeholder="Choose starting point" />
-                    <AiOutlineSearch className=" absolute right-1 top-1/2 -translate-y-1/2" />
-                    </div>
+        <div className="flex">
 
-                </div>
-                <div className="flex items-center justify-between gap-1 mt-3 ">
-                    <span><BiCurrentLocation className="text-red-800" /></span>
-                    <div className="flex-1 rounded-md overflow-hidden relative">
-                    <input type="text" className="border text-xs p-3 w-full  bg-gray-100  outline-none" placeholder="Choose Destination" />
-                    <AiOutlineSearch className=" absolute right-1 top-1/2 -translate-y-1/2" />
-                    </div>
-
-                </div>
+          <div className="mt-3 flex-1">
+            <div className="flex items-center justify-between gap-1 ">
+              <span><HiOutlineLocationMarker className="text-blue-800" /></span>
+              <div className="flex-1 rounded-md overflow-hidden relative">
+                <input type="text" className="border text-xs p-3 w-full  bg-gray-100  outline-none" placeholder="Choose starting point" />
+                <AiOutlineSearch className=" absolute right-1 top-1/2 -translate-y-1/2" />
+              </div>
 
             </div>
-            <div className='mt-3 flex justify-center text-gray-700 items-center text-3xl'>
-                <HiOutlineSwitchVertical/>
+            <div className="flex items-center justify-between gap-1 mt-3 ">
+              <span><BiCurrentLocation className="text-red-800" /></span>
+              <div className="flex-1 rounded-md overflow-hidden relative">
+                <input type="text" className="border text-xs p-3 w-full  bg-gray-100  outline-none" placeholder="Choose Destination" />
+                <AiOutlineSearch className=" absolute right-1 top-1/2 -translate-y-1/2" />
+              </div>
+
             </div>
-            
+
+          </div>
+          <div className="mt-3 flex justify-center text-gray-700 items-center text-3xl">
+            <HiOutlineSwitchVertical />
+          </div>
+
         </div>
 
       </div>
