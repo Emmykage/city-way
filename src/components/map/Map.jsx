e3b8aabe3c21a45c4cb1c17c18623c9a29d159e0
morris menanya <emmiemenz@gmail.com>
2023-10-29 11:22:20 +0100

    add geolocation

--- src/components/map/Map.jsx
+++ src/components/map/Map.jsx
@@ -2,12 +2,14 @@ import React, { useEffect, useState } from 'react';
 import GoogleMapReact from 'google-map-react';
 
 const Map = () => {
-  const [coordinates, setCoordinates] = useState({});
+  const [coordinates, setCoordinates] = useState({ lat: 9.059676306288111, lng: 7.47815996115190 });
   useEffect(() => {
     navigator.geolocation.getCurrentPosition(({ coords: { latitude, longitude } }) => {
       setCoordinates({ lat: latitude, lng: longitude });
     });
   }, []);
+  console.log(coordinates);
+
   return (
     <div className="w-full z-0 left-0 h-full">
       <GoogleMapReact
